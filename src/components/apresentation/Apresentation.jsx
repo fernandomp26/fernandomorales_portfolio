b35052925995794f28701097335732c9d8ce8f2a
Fernando Morales Peres <142750804+fernandomp26@users.noreply.github.com>
2024-06-15 03:06:28 +0000

    textos de apresentação alterados

--- src/components/apresentation/Apresentation.jsx
+++ src/components/apresentation/Apresentation.jsx
@@ -11,9 +11,9 @@ export default function Apresentation() {
                <img src={photo} alt="Profile Img" width={400} />
             </div>
             <div className="main-text">
-               <h1 className='hello'>Hello, my name is</h1>
-               <h1 className='name'><strong>Fernando</strong>!</h1>
-               <p>This Portfolio tells a little about me and my life experiences. <br></br>Bellow you can send me a mesage on Whatsapp, follow me on Instagram and acess my GitHub!</p>
+               <h1 className='hello'>Hello, I'm</h1>
+               <h1 className='name'>&lt;<strong>Fernando</strong>/&gt;</h1>
+               <p>A great admirer of front-end and back-end programming, UI/UX design, and currently a university student <br></br>in Information Systems.</p>
                <div className="social-media">
                   <a href="https://github.com/fernandomp26" target='_blank'>
                      <FaGithub />
